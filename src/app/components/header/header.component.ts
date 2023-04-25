9af2780c8026429f9f054b9d31ad74dbc2c4225a
clioteorica <clioteorica@gmail.com>
2023-04-25 20:39:36 -0300

    cambios en el login

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { PortfolioService } from 'src/app/services/portfolio.service';
+import { Router } from '@angular/router';
+import { persona } from 'src/app/model/persona.model';
+import { PersonaService } from 'src/app/services/persona.service';
+import { TokenService } from 'src/app/services/token.service';
+// import { PortfolioService } from 'src/app/services/portfolio.service';
 
 @Component({
   selector: 'app-header',
@@ -7,14 +11,36 @@ import { PortfolioService } from 'src/app/services/portfolio.service';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  miPortfolio:any;
-  constructor(private datosPortfolio:PortfolioService) { }
+  persona: persona = new persona("", "", "", "", "");
+  isLogged = false;
+  // miPortfolio:any;
+  constructor(public personaService: PersonaService, private router:Router, private tokenService: TokenService
+    /*private datosPortfolio:PortfolioService*/) { }
 
-  ngOnInit(): void {
-    this.datosPortfolio.obtenerDatos().subscribe(data =>{
-      console.log(data)
-      this.miPortfolio = data;
-    });
+  ngOnInit(): void
+  {
+    this.personaService.getPersona().subscribe(data => { this.persona = data })
+    if (this.tokenService.getToken()) {
+      this.isLogged = true;
+    } else {
+      this.isLogged = false;
+    }
+    // this.datosPortfolio.obtenerDatos().subscribe(data =>{
+    //   console.log(data)
+    //   this.miPortfolio = data;
+    // });
   }
+
+  onLogOut(): void
+  {
+    this.tokenService.logOut();
+    window.location.reload();
+  }
+
+  login()
+  {
+    this.router.navigate(['/login'])
+  }
+
 
 }
